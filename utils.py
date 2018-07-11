1543f1e449a7988fcc17839504e1a1e2392f6a32
Dwight House <dwighthouse@gmail.com>
2018-07-11 18:16:22 -0500

    Simplified the number conversion.

--- utils.py
+++ utils.py
@@ -14,16 +14,15 @@ def context_func(app, win):
 number_conversions = {
     'oh': '0', # 'oh' => zero
 }
-for i, w in enumerate(['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',]):
+for i, w in enumerate(['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']):
     number_conversions[str(i)] = str(i)
     number_conversions[w] = str(i)
     number_conversions['%s\\number'%(w)] = str(i)
 
+# TODO: Once implemented, use number input value rather than manually parsing number words with this function
 def parse_words_as_integer(words):
-    # TODO: Once implemented, use number input value rather than manually parsing number words with this function
-
     # Ignore any potential trailing non-number words
-    number_words = list(itertools.takewhile(lambda w: w not in number_conversions, words))
+    number_words = list(itertools.takewhile(lambda w: str(w) in number_conversions, words))
 
     # Somehow, no numbers were detected
     if len(number_words) == 0:
@@ -32,18 +31,11 @@ def parse_words_as_integer(words):
     # Map number words to simple number values
     number_values = list(map(lambda w: number_conversions[w.word], number_words))
 
-    # Filter out initial zero values
-    normalized_number_values = []
-    non_zero_found = False
-    for n in number_values:
-        if not non_zero_found and n == '0':
-            continue
-        non_zero_found = True
-        normalized_number_values.append(n)
+    # Join the numbers into single string, and remove leading zeros
+    number_string = ''.join(number_values).lstrip('0')
 
     # If the entire sequence was zeros, return single zero
-    if len(normalized_number_values) == 0:
-        normalized_number_values = ['0']
+    if len(number_string) == 0:
+        return 0
 
-    # Create merged number string and convert to int
-    return int(''.join(normalized_number_values))
+    return int(number_string)
