5c6ed2cea1a419ba276419e7ef9ff21d04763190
Dwight House <dwighthouse@gmail.com>
2018-07-12 18:22:36 -0500

    In talon_control, improved some command wording, added the ability to open the log in Console, and added the ability to turn voice debug logging on and off.

--- talon_control.py
+++ talon_control.py
@@ -5,14 +5,15 @@
 
 # WARNING: Because this script uses ContextGroup, changes to the script may not be recognized until a Talon restart.
 
-from talon import app
+from talon import app, applescript
 from talon.api import lib
+from talon.engine import engine
 from talon.voice import Context, ContextGroup, talon
 import eye_mouse
 
 # Because the Voice Commands on this Context are in their own ContextGroup, they are treated separatedly from the other ContextGroups (including the default one: talon)
 # By disabling the default talon ContextGroup, we can effectively turn off recognition, save for the Voice Commands in this file
-def set_enabled(enable):
+def set_voice_enabled(enable):
     if enable:
         talon.enable()
         app.icon_color(0, 0.7, 0, 1)
@@ -25,15 +26,49 @@ def set_enabled(enable):
 # Creates extra menu item for toggling Speech Recognition
 def on_menu(item):
     if item == '!Enable Speech Recognition':
-        set_enabled(not talon.enabled)
+        set_voice_enabled(not talon.enabled)
 
 app.register('menu', on_menu)
 
 
-# Respect the enabled-ness of default talon ContextGroup for these Voice Commands
+
+# Adapted from debug.py listed in the Slash channel
+def debug_listener(topic, m):
+    if topic == 'cmd' and m['cmd']['cmd'] == 'g.load' and m['success'] == True:
+        print('[grammar reloaded]')
+    else:
+        print(topic, m)
+
+is_debug_enabled = False
+
+def set_debug_enabled(enable):
+    global is_debug_enabled
+
+    # Respect the enabled-ness of default talon ContextGroup for these Voice Commands
+    # Also, no point in re-registering
+    if not talon.enabled or enable == is_debug_enabled:
+        return
+
+    if enable:
+        engine.register('', debug_listener)
+    else:
+        engine.unregister('', debug_listener)
+
+    is_debug_enabled = enable
+
+
+
+def open_debug_log(m):
+    # Opens the Talon logs in Console.app, which is where print statements and debugging data is usually sent unless Repl is active
+    applescript.run(f'do shell script "open ~/.talon/talon.log"')
+
+
+
 def on_eye_control(menu_string):
+    # Respect the enabled-ness of default talon ContextGroup for these Voice Commands
     if talon.enabled:
         eye_mouse.on_menu(menu_string)
+
 
 
 context_group = ContextGroup('talon_control')
@@ -41,22 +76,25 @@ context = Context('talon_control', group=context_group)
 
 context.keymap({
     # Enable/disable voice recognition
-    'talon sleep': lambda m: set_enabled(False),
-    'talon wake': lambda m: set_enabled(True),
+    'talon [voice] sleep': lambda m: set_voice_enabled(False),
+    'talon [voice] wake': lambda m: set_voice_enabled(True),
+
+    # Enable/disable debug logging to console
+    'talon [voice] debugging on': lambda m: set_debug_enabled(True),
+    'talon [voice] debugging off': lambda m: set_debug_enabled(False),
+
+    # Open talon.log in Console.app
+    'talon show log': open_debug_log,
 
     # Toggle various eye tracking systems
     'talon (calibrate | calibration)': lambda m: on_eye_control('Eye Tracking >> Calibrate'),
     'talon mouse [control]': lambda m: on_eye_control('Eye Tracking >> Control Mouse'),
     'talon zoom [mouse]': lambda m: on_eye_control('Eye Tracking >> Control Mouse (Zoom)'), # Currently doesn't work for unknown reasons
-    'talon keyboard': on_eye_control('Eye Tracking >> Keyboard'), # Currently doesn't work for unknown reasons
+    'talon keyboard': lambda m: on_eye_control('Eye Tracking >> Keyboard'), # Currently doesn't work for unknown reasons
     'talon eye debug': lambda m: on_eye_control('Eye Tracking >> Show Debug Overlay'),
     'talon eye camera': lambda m: on_eye_control('Eye Tracking >> Show Camera Overlay'),
-
-    # TODO: Enable/Disable the CLI debug info
-    # TODO: Open Console to talon.log at "Now"
-    # TODO: Restart Talon (for reloading ContextGroups)
 })
 
 # Startup
-set_enabled(talon.enabled)
+set_voice_enabled(talon.enabled)
 context_group.load()
